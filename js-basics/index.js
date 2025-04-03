687ed1d9ff559fba3d2ee852fed9b5e23a0c71ec
aser-tesfaye <asertesfaye@hotmail.com>
2025-04-03 10:17:44 +0300

    js bigginer tutorial with mosh

---
+++ js-basics/index.js
@@ -0,0 +1,63 @@
+// // This is my first javascript code!
+// console.log("Hello World");
+
+// let name = "John Doe";
+// console.log(name);
+
+// // cannot be a reserved word
+// // should be meaningful
+// // cannot start with a number (1name)
+// // cannot contain a space or hyphen (-)
+// // are case-sensitive
+
+// let firstName = "John";
+
+// let interestRate = 0.3;
+// interestRate = 1;
+// console.log(interestRate);
+
+// let name = 'Aser'; //String Literal
+// let age = 30; //Number Literal
+// let isApproved = true; //Boolean Literal
+// let firstName = undefined; //Undefined
+// let lastName = null; //Null Literal
+
+// ---------------------------------------
+// let name = "John";
+// let age = 30;
+// let person = {
+//     name: "John",
+//     age: 30
+// };
+// // Dot notation
+// person.name = "Mary";
+// person.age = 25;
+
+// // Bracket notation
+// let selection = 'name';
+// person[selection] = "Aser";
+
+// console.log(person);
+
+// --------------------------------------
+
+// let selectedColors = ['red', 'blue'];
+// selectedColors[2] = 3;
+// console.log(selectedColors.length);
+
+// --------------------------------------
+
+// Performing a task
+function greet(name, lastname) {
+    console.log('Hello ' + name + ' ' + lastname);
+}
+
+// greet('Aser');
+// greet('John');
+
+// Calculate a value
+function square(number) {
+    return number * number;
+}
+
+console.log(square(2));
